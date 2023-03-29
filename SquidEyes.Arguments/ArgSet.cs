f3c2b9b320bc4c57701e89e48435ef3e17e23053
Louis S. Berman <louis@squideyes.com>
2023-03-29 17:15:23 -0400

    Develop (#2)
    * Updated ArgSet
    
    * Updated ArgSet (Round 2)
    
    * More work on ArgSet

--- SquidEyes.Arguments/ArgSet.cs
+++ SquidEyes.Arguments/ArgSet.cs
@@ -1,7 +1,12 @@
-﻿namespace SquidEyes.Arguments
-{
-    public class ArgSet
-    {
+﻿namespace SquidEyes.Arguments;
 
+public class ArgSet
+{
+    private readonly Dictionary<string, object> args = new();
+
+    public void Add<T>(string name, T value)
+        where T : IParsable<T>
+    {
+        args[name] = value;
     }
 }
